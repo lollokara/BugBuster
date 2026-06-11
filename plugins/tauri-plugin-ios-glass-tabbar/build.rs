decad289f54ff983f08380d5b1d9dcc8e3490cc3
lollokara <lollo.kl@hotmail.it>
2026-06-12 00:18:51 +0200

    Removed

--- plugins/tauri-plugin-ios-glass-tabbar/build.rs
+++
@@ -1,8 +0,0 @@
-const COMMANDS: &[&str] = &["set_items", "set_active_tab", "set_hidden", "set_badge"];
-
-fn main() {
-    tauri_plugin::Builder::new(COMMANDS)
-        .android_path("android")
-        .ios_path("ios")
-        .build();
-}
--- plugins/tauri-plugin-ios-glass-tabbar/ios/README.md
+++
@@ -1,3 +0,0 @@
-# Tauri Plugin ios-glass-tabbar
-
-A description of this package.
